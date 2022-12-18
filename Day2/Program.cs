6da6146d91cdce17c01698654a2225c0187294f0
SuperArcherG <JudahBertou@gmail.com>
2022-12-17 20:00:55 -0500

    Part 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace Day2
 {
@@ -13,13 +12,25 @@ namespace Day2
             Paper = 2,
             Scissors = 3
         }
-        enum Awards
+        enum Award
         {
             Loss = 0,
             Draw = 3,
             Win = 6,
             TF = -1
         }
+        static Dictionary<string, string> cheat = new Dictionary<string, string>()
+        {
+                { "A X", "C"},
+                { "A Y", "A"},
+                { "A Z", "B"},
+                { "B X", "A"},
+                { "B Y", "B"},
+                { "B Z", "C"},
+                { "C X", "B"},
+                { "C Y", "C"},
+                { "C Z", "A"}
+        };
 
         static readonly string inputPath = Directory.GetCurrentDirectory().Replace("Day2/bin/Debug/netcoreapp3.1", "") + "Input.txt"; //The directory of your input text file, create an Input.txt at your "System.AppContext.BaseDirectory" folder
 
@@ -45,7 +56,18 @@ namespace Day2
             {
                 total += score;
             }
-            Console.WriteLine(total);
+            Console.WriteLine("Pt. 1: " + total);
+
+            total = 0;
+            foreach (string line in lines)
+            {
+                string[] moves = line.Split(" ");
+                int bonus = (int)StringToAward(moves[1]);
+                string response = cheat[line];
+                int _base = (int)StringToRPS(response);
+                total += _base + bonus;
+            }
+            Console.WriteLine("Pt. 2: " + total);
         }
 
         static int PointsAwarded(RPS Elf, RPS You)
@@ -53,32 +75,32 @@ namespace Day2
             if (Elf == You)
             {
                 Console.Write("Draw | ");
-                return (int)Awards.Draw + (int)You;
+                return (int)Award.Draw + (int)You;
             }
 
             if (Elf < You && Elf != You - 2 || You == Elf - 2)
             {
                 Console.Write("You Win | ");
-                return (int)Awards.Win + (int)You;
+                return (int)Award.Win + (int)You;
             }
 
             if (Elf > You || You == Elf + 2)
             {
                 Console.Write("You loose | ");
-                return (int)Awards.Loss + (int)You;
+                return (int)Award.Loss + (int)You;
             }
 
             if (Elf != You - 2)
             {
                 Console.Write("You Win | ");
-                return (int)Awards.Win + (int)You;
+                return (int)Award.Win + (int)You;
             }
 
 
             else
             {
                 Console.Write("U broke smthn | ");
-                return (int)Awards.TF + (int)You;
+                return (int)Award.TF + (int)You;
             }
         }
         static RPS StringToRPS(string input)
@@ -86,6 +108,13 @@ namespace Day2
             if (input.Contains("A") || input.Contains("X")) return RPS.Rock;
             else if (input.Contains("B") || input.Contains("Y")) return RPS.Paper;
             else if (input.Contains("C") || input.Contains("Z")) return RPS.Scissors;
+            else throw null;
+        }
+        static Award StringToAward(string input)
+        {
+            if (input.Contains("X")) return Award.Loss;
+            else if (input.Contains("Y")) return Award.Draw;
+            else if (input.Contains("Z")) return Award.Win;
             else throw null;
         }
     }
